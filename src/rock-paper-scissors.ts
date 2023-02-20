25707092dc51a2c7ea48dfd8a00adf62245bfde9
henrybirch <hab.b@hotmail.co.uk>
2023-02-20 15:29:20 +0000

    Add working game

--- src/rock-paper-scissors.ts
+++ src/rock-paper-scissors.ts
@@ -1,7 +1,111 @@
 enum Outcome {
-    Win = "Win", Draw = "Draw", Lose = "Lose"
+    Win = 1, Draw = 0, Lose = -1
 }
 
 enum Choice {
-    Paper = "Paper", Rock = "Rock", Scissors = "Scissors"
+    Rock = "Rock", Paper = "Paper", Scissors = "Scissors"
 }
+
+function getValueOfChoice(choice: Choice) {
+    switch (choice) {
+        case Choice.Paper:
+            return 0
+        case Choice.Rock:
+            return 1
+        case Choice.Scissors:
+            return 2
+    }
+}
+
+function getOutcomeOfRound(playerChoice: Choice, computerChoice: Choice) {
+    if (playerChoice == computerChoice) return Outcome.Draw
+    if ((getValueOfChoice(playerChoice) - getValueOfChoice(computerChoice)) == -1) return Outcome.Win
+    return Outcome.Lose
+}
+
+function getDeclarationOfRound(playerChoice: Choice, computerChoice: Choice) {
+    switch (getOutcomeOfRound(playerChoice, computerChoice)) {
+        case Outcome.Draw:
+            return "You draw!" + " You both chose " + playerChoice.toString().toLowerCase() + "!"
+        case Outcome.Win:
+            return "You win! " + playerChoice.toString() + " beats " + computerChoice.toString().toLowerCase() + "!"
+        case Outcome.Lose:
+            return "You lose! " + playerChoice.toString() + " loses to " + computerChoice.toString().toLowerCase() + "!"
+    }
+}
+
+type Game = Outcome[]
+
+function getOutcomeOfGame(game: Game) {
+    const sum = game.reduce((previousValue, currentValue) => previousValue + currentValue.valueOf())
+    if (sum > 0) return Outcome.Win
+    if (sum < 0) return Outcome.Lose
+    return Outcome.Draw
+}
+
+type Stats = {
+    wins: Number
+    losses: Number
+    draws: Number
+}
+
+function getStats(game: Game): Stats {
+    return {
+        wins: game.filter(outcome => outcome == Outcome.Win).length,
+        losses: game.filter(outcome => outcome == Outcome.Lose).length,
+        draws: game.filter(outcome => outcome == Outcome.Draw).length
+    }
+}
+
+function statsToString(stats: Stats): String {
+    return "Wins: " + stats.wins + "\n" + "Losses: " + stats.wins + "\n" + "Draws: " + stats.draws
+}
+
+function getDeclarationOfGame(game: Game): String {
+    const outcome = getOutcomeOfGame(game)
+    const stats = getStats(game)
+    switch (outcome) {
+        case Outcome.Draw:
+            return "The game is a draw!" + "\n" + statsToString(stats)
+        case Outcome.Win:
+            return "You won the game!" + "\n" + statsToString(stats)
+        case Outcome.Lose:
+            return "You lost the game!" + "\n" + statsToString(stats)
+    }
+}
+
+function getChoice(): Choice {
+    const choice = prompt("Enter rock, paper or scissors: ")
+    switch (choice.trim().toLowerCase()) {
+        case "rock":
+            return Choice.Rock
+        case "paper":
+            return Choice.Paper
+        case "scissors":
+            return Choice.Scissors
+        default:
+            throw new Error("Not a choice!")
+    }
+}
+
+function getComputerChoice(): Choice {
+    const choices = [Choice.Rock, Choice.Paper, Choice.Scissors]
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function playGame(n) {
+    function go(game: Game, n: number) {
+        if (n == 0) {
+            alert(getDeclarationOfGame(game))
+        } else {
+            const playerChoice = getChoice()
+            const computerChoice = getComputerChoice()
+            alert(getDeclarationOfRound(playerChoice, computerChoice))
+            go(game.concat(getOutcomeOfRound(playerChoice, computerChoice)), n - 1)
+        }
+    }
+
+    go(Array(), n)
+}
+
+playGame(5)
